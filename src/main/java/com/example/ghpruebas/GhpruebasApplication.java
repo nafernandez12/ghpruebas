a45c0d0987a1cdb21f86cc8a2daa9f48c25ad5d6
Nahuel Fernandez <na_fernandez@live.com.ar>
2022-05-18 20:55:36 -0300

    adding spring files

---
+++ src/main/java/com/example/ghpruebas/GhpruebasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghpruebas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhpruebasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhpruebasApplication.class, args);
+	}
+
+}
